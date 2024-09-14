0d1e3454985f2cf59ba59a65a91d03cf9117dd3e
banupriyatamilarasan <banupriyasr90@gmail.com>
2024-09-14 20:31:01 +0530

    found

---
+++ src/test/java/org/com/ManualT.java
@@ -0,0 +1,14 @@
+package org.com;
+
+public class ManualT {
+	private void staff() {
+		System.out.println("r");
+	}
+	private void staff1() {
+		System.out.println("m");
+	}
+	private void staff2() {
+		System.out.println("t");
+	}
+
+}
